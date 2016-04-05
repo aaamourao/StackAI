04c53f6b929484030c3ffa182731338126b73e22
adriano <adrianomourao@rotonmail.com>
2016-04-05 01:50:13 -0300

    MySQL django setup and ignoring confidencial database.cnf

--- src/core/settings.py
+++ src/core/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'database.cnf',
+        },
     }
 }
 
