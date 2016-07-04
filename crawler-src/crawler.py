89cd103b98f8c7a4a583652acf1f7179817c2ead
Vinícius Garcia <vingarcia00@gmail.com>
2016-07-04 05:54:56 -0300

    Update crawler

--- crawler-src/crawler.py
+++ crawler-src/crawler.py
@@ -27,12 +27,11 @@ def getInner(html):
   return innerHtml.match(html).group(1)
 
 def xmlSet(xml, attr, value):
-  if not 'unicode' in globals():
-    unicode = str
-  if type(value) == unicode or type(value) == str:
+  if type(value).__name__ in [ 'unicode', 'str' ]:
     xml.set(attr, value)
 
 uid_re = re.compile("users/([0-9]+)/[^/]+\Z")
+qid_re = re.compile("questions/([0-9]+)/[^/]+\Z")
 
 class VidSpider(scrapy.Spider):
   # Required attributes:
@@ -53,9 +52,12 @@ class VidSpider(scrapy.Spider):
   users = etree.Element('users')
 
   # Pre-load some files/data:
-  def __init__(self, uid=None):
+  def __init__(self, uid=None, refreshQuestions=True):
+    # Toggle when to crawl for questions as well:
+    self.refreshQuestions = refreshQuestions == True
+
     if uid:
-      self.users_url = [ "http://superuser.com/users/%s/?tab=tags" ]
+      self.users_url = [ "http://superuser.com/users/%s/" % uid ]
 
   count = 0
   
@@ -65,43 +67,108 @@ class VidSpider(scrapy.Spider):
   # parsed by other function.
   def parse(self, resp):
 
-    for link in resp.css('#mainbar a.question-hyperlink::attr(href)').extract():
-      if self.count == MAX_QUESTIONS:
-          break
-      else:
-          self.count += 1
-
-      # Schedule it to be parsed:
-      yield scrapy.Request(
-          resp.urljoin(link),
-          callback=partial(self.parseQuestion)
-        )
-
     for link in self.users_url:
       # Schedule it to be parsed:
       yield scrapy.Request(
-          link+"?tab=tags", callback=partial(self.parseUser)
+          link, callback=partial(self.parseUser)
         )
+
+    if self.refreshQuestions:
+      for link in resp.css('#mainbar a.question-hyperlink::attr(href)').extract():
+        if self.count == MAX_QUESTIONS:
+            break
+        else:
+            self.count += 1
+
+        # Schedule it to be parsed:
+        yield scrapy.Request(
+            resp.urljoin(link),
+            callback=partial(self.parseQuestion)
+          )
 
   def parseUser(self, resp):
     user = etree.Element('row')
 
     xmlSet(user, 'Id', uid_re.findall(resp.url)[0])
-    xmlSet(user, 'Name', resp.css("div.name::text").extract_first().strip())
+    xmlSet(user, 'DisplayName', resp.css("h2.user-card-name::text").extract_first().strip())
+    xmlSet(user, 'Reputation', re.sub('[^0-9]', '', resp.css('div.reputation::text').extract_first()))
+    xmlSet(user, 'CreationDate', resp.css('.user-links li span[title]:not([class])::attr(title)').extract_first())
+    xmlSet(user, 'UpVoted', "0")
+    xmlSet(user, 'DownVoted', "0")
 
-    tags = ""
+    n_answers = int(resp.css('.user-stats .answers .number::text').extract_first())
+    n_questions = int(resp.css('.user-stats .questions .number::text').extract_first())
+    xmlSet(user, 'NumPosts', str(n_answers+n_questions))
+
+    # Crawl for all user tags info:
+    xmlSet(user, 'Tags', '')
+    yield scrapy.Request(
+        resp.url+"?tab=tags", callback=partial(self.parseTags, user)
+      )
+
+    # Crawl for user up/down votes info:
+    yield scrapy.Request(
+        resp.url+"?tab=topactivity", callback=partial(self.parseVotes, user)
+      )
+
+    # Crawl for user total number of comments:
+    yield scrapy.Request(
+        resp.url+"?tab=activity&sort=comments", callback=partial(self.parseNumComments, user)
+      )
+
+    # Save that post:
+    self.users.append(user)
+
+  def parseVotes(self, user, resp):
+
+    votes_sel = list(resp.css('.votes-cast-stats tr'))
+
+    if len(votes_sel) == 0:
+      xmlSet(user, 'UpVotes', '0')
+      xmlSet(user, 'DownVotes', '0')
+    else:
+      xmlSet(user, 'UpVotes', votes_sel[1].css('td::text').extract_first())
+      xmlSet(user, 'DownVotes', votes_sel[2].css('td::text').extract_first())
+
+  def parseNumComments(self, user, resp):
+    total = resp.css('h1 span.count').extract_first()
+    xmlSet(user, 'NumComments', re.sub('[^0-9]', '', total))
+
+  def parseBadges(self, user, resp):
+    tags = user.get('Tags')
     for sel in resp.css(".user-tags td"):
       tag_name = sel.css("a.post-tag::text").extract_first()
       tag_count = sel.css(".item-multiplier-count::text").extract_first() or 1
       tags += (("<%s>" % tag_name) * int(tag_count))
     xmlSet(user, 'Tags', tags)
 
-    # Save that post:
-    self.users.append(user)
+    # TODO: If the internet stops hanging we could uncomment this
+    # so that we would get all the user tags, instead of just the most
+    # common ones.
+    #next_page = resp.css('a[href][rel="next"]::attr(href)').extract_first()
+    #if next_page:
+    #  yield scrapy.Request(
+    #      resp.urljoin(next_page), callback=partial(self.parseTags, user)
+    #    )
+
+  def parseTags(self, user, resp):
+    tags = user.get('Tags')
+    for sel in resp.css(".user-tags td"):
+      tag_name = sel.css("a.post-tag::text").extract_first()
+      tag_count = sel.css(".item-multiplier-count::text").extract_first() or 1
+      tags += (("<%s>" % tag_name) * int(tag_count))
+    xmlSet(user, 'Tags', tags)
+
+    next_page = resp.css('a[href][rel="next"]::attr(href)').extract_first()
+    if next_page:
+      yield scrapy.Request(
+          resp.urljoin(next_page), callback=partial(self.parseTags, user)
+        )
 
   def parseQuestion(self, resp):
     post = etree.Element('row')
 
+    xmlSet(post, 'Id', qid_re.findall(resp.url)[0])
     xmlSet(post, 'url', resp.url)
     xmlSet(post, 'PostTypeId', "1")
     xmlSet(post, 'AcceptedAnswerId',
@@ -160,22 +227,23 @@ class VidSpider(scrapy.Spider):
   # Save the data before quit:
   def closed(self, reason):
 
-    # To write utf-8 files do as described bellow:
-    #with codecs.open('data/Posts.xml', 'w', 'utf-8') as file:
-    with open('data/Posts.xml', 'wb') as file:
-      # text = json.dumps(
-      #     self.pages,
-      #     indent = 2,
-      #     sort_keys=True).decode('raw_unicode_escape')
+    if self.refreshQuestions:
+      # To write utf-8 files do as described bellow:
+      #with codecs.open('data/Posts.xml', 'w', 'utf-8') as file:
+      with open('data/Posts.xml', 'wb') as file:
+        # text = json.dumps(
+        #     self.pages,
+        #     indent = 2,
+        #     sort_keys=True).decode('raw_unicode_escape')
 
-      xml_text = etree.tostring(
-        self.posts, encoding='utf-8', pretty_print=True, xml_declaration=True)
-      file.write(xml_text)
+        xml_text = etree.tostring(
+          self.posts, encoding='utf-8', pretty_print=True, xml_declaration=True)
+        file.write(xml_text)
 
-      # Using xmldump:
-      # header = '<?xml version="1.0" encoding="utf-8"?>\n'
-      # text = xmldump.dumps({ 'row': self.pages }, 'pages', indent=2)
-      # file.write(header + text)
+        # Using xmldump:
+        # header = '<?xml version="1.0" encoding="utf-8"?>\n'
+        # text = xmldump.dumps({ 'row': self.pages }, 'pages', indent=2)
+        # file.write(header + text)
 
     with open('data/Users.xml', 'wb') as file:
       xml_text = etree.tostring(
